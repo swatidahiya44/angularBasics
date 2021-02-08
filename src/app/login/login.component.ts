f17021860b9c00b0b9ce80cae2cae1115f51171f
swatidahiya44 <swatidahiya44@gmail.com>
2021-02-08 13:08:09 +0530

    First-commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,13 +10,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: any;
+  password: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) { 
+    
+  }
 
   ngOnInit(): void {
   }
 
-  login=()=>{
-    this.router.navigate(['/home']);
+  login (){
+    console.log(this.email)
+    var a = "swati@gmail.com";
+    var b = "pretty";
+    if(a === this.email && b === this.password){
+      this.router.navigate(['/home']);
+
+    }
+    else{
+      alert("Invalid Email or Password");
+      this.email = "";
+      this.password = "";
+    }
+
+
+  
+    
   }
 }
